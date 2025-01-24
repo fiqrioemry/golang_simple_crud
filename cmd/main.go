b4a9a3781af732d65f57d372f8f73e540219a174
fiqrioemry <foemry@gmail.com>
2025-01-24 17:43:10 +0700

    update changes

--- cmd/main.go
+++ cmd/main.go
@@ -11,32 +11,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main(){
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Warning : .env file not found %v", err)
-	}
-
-	database.ConnectDatabase()
-
-	router := mux.NewRouter()
-
-	router.HandleFunc("/register/seeker", handlers.SeekerRegister).Methods("POST")
-	router.HandleFunc("/register/employer", handlers.EmployerRegister).Methods("POST")
-	router.HandleFunc("/login", handlers.EmployerRegister).Methods("POST")
-
-	port := os.Getenv()
-	if port == ""{
-		port = "5000"
-	}
-
-	log.Printf("Starting server on :%s...", port)
-	 	if err := http.ListenAndServe(":"+port, router); err != nil {
-	 		log.Fatalf("Failed to start server: %v", err)
-	}
-	
-
-}
 
 func main(){
 	err := godotenv.Load()
@@ -50,7 +24,7 @@ func main(){
 
 	router.HandleFunc("/register/seeker", handlers.SeekerRegister).Methods("POST")
 	router.HandleFunc("/register/employer", handlers.EmployerRegister).Methods("POST")
-	router.HandleFunc("/login", handlers.EmployerRegister).Methods("POST")
+	router.HandleFunc("/login", handlers.Login).Methods("POST")
 
 	port := os.Getenv("PORT")
 	if port == ""{
@@ -61,6 +35,5 @@ func main(){
 	 	if err := http.ListenAndServe(":"+port, router); err != nil {
 	 		log.Fatalf("Failed to start server: %v", err)
 	}
-	
 
 }
