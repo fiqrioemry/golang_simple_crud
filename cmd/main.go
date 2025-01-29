90fc95e3bd943ae6abcea222855bec2cd206a3ed
fiqrioemry <foemry@gmail.com>
2025-01-29 10:07:09 +0700

    update changes

--- cmd/main.go
+++ cmd/main.go
@@ -10,38 +10,41 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
-	// load env file
+	// Load env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Warning : .env file not found %v", err)
 	}
 
-	// initiate database
+	// Initiate database
 	database.ConnectDatabase()
 
-	// create route api
+	// Create router
 	router := mux.NewRouter()
+
+	// Middleware API Key
 	router.Use(middleware.APIKeyMiddleware)
 
-	// authentication
+	// Authentication Routes
 	router.HandleFunc("/api/login", handlers.Login).Methods("POST")
 	router.HandleFunc("/api/refresh", handlers.GetRefreshToken).Methods("POST")
 	router.HandleFunc("/api/register/seeker", handlers.SeekerRegister).Methods("POST")
 	router.HandleFunc("/api/register/employer", handlers.EmployerRegister).Methods("POST")
 
-	// public access
+	// Public Routes
 	router.HandleFunc("/api/jobs", handlers.GetAllJobs).Methods("GET")
 	router.HandleFunc("/api/jobs/{id}", handlers.GetJobByID).Methods("GET")
 
-	// Protected endpoint with middleware
+	// Protected Routes with JWT Middleware
 	protected := router.PathPrefix("/").Subrouter()
 	protected.Use(middleware.JWTMiddleware)
 	protected.HandleFunc("/api/me", handlers.AuthMe).Methods("POST")
 
-	// seeker
+	// Seeker Routes
 	protected.HandleFunc("/api/jobs/{id}/apply", handlers.ApplyToJob).Methods("POST")
 	protected.HandleFunc("/api/seeker/profile", handlers.GetUserSeekerProfile).Methods("GET")
 	protected.HandleFunc("/api/seeker/profile", handlers.UpdateUserSeekerProfile).Methods("PUT")
@@ -49,10 +52,7 @@ func main() {
 	protected.HandleFunc("/api/seeker/profile/experience", handlers.AddUserSeekerExperience).Methods("POST")
 	protected.HandleFunc("/api/seeker/profile/experience/{id}", handlers.UpdateUserSeekerExperience).Methods("PUT")
 
-	// next feature
-	// protected.HandleFunc("/api/seeker/applications/{id}", handlers.CancelSeekerJobApplication).Methods("DELETE")
-
-	// employer
+	// Employer Routes
 	protected.HandleFunc("/api/employer/jobs", handlers.CreateJob).Methods("POST")
 	protected.HandleFunc("/api/employer/jobs/{id}", handlers.UpdateJob).Methods("PUT")
 	protected.HandleFunc("/api/employer/jobs/{id}", handlers.DeleteJob).Methods("DELETE")
@@ -60,14 +60,29 @@ func main() {
 	protected.HandleFunc("/api/employer/jobs/{id}/applications", handlers.UpdateApplicationStatus).Methods("PUT")
 	protected.HandleFunc("/api/employer/jobs/{id}/applications", handlers.GetEmployerJobApplications).Methods("GET")
 
+	// cors configuration
+	client_host := os.Getenv("CLIENT_HOST")
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{client_host},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization", "X-API-key"},
+		AllowCredentials: true,
+	})
+
+	router.Methods(http.MethodOptions).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	handler := corsHandler.Handler(router)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
 	log.Printf("Starting server on :%s...", port)
-	if err := http.ListenAndServe(":"+port, router); err != nil {
+	if err := http.ListenAndServe(":"+port, handler); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
-
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
