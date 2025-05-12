f4c53edf0d627b8f18e8807f52aeaa9d55463977
fiqrioemry <foemry@gmail.com>
2025-05-12 22:15:37 +0700

    update change

--- server/internal/models/models.go
+++ server/internal/models/models.go
@@ -24,3 +24,40 @@ type Token struct {
 	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
+
+type UserSubscription struct {
+	ID                 uint      `gorm:"primaryKey"`
+	UserID             uuid.UUID `gorm:"type:uuid;not null;uniqueIndex"` // hanya 1 aktif per user
+	SubscriptionTierID uint      `gorm:"not null"`
+	StartedAt          time.Time `gorm:"not null"`
+	ExpiresAt          time.Time `gorm:"not null"`
+	IsActive           bool      `gorm:"default:true"`
+
+	User             User             `gorm:"foreignKey:UserID"`
+	SubscriptionTier SubscriptionTier `gorm:"foreignKey:SubscriptionTierID"`
+}
+
+type SubscriptionTier struct {
+	ID          uint    `gorm:"primaryKey"`
+	Name        string  `gorm:"type:varchar(50);uniqueIndex;not null"`
+	TokenLimit  int     `gorm:"not null"`
+	Price       float64 `gorm:"not null"`
+	Duration    int     `gorm:"not null"`
+	Description string  `gorm:"type:text"`
+}
+
+type Payment struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null;index"`
+	TierID    uint      `gorm:"not null"`
+	Subtotal  float64   `gorm:"not null"`
+	Tax       float64   `gorm:"not null"`
+	Total     float64   `gorm:"not null"`
+	Method    string    `gorm:"type:varchar(50);not null"`
+	Status    string    `gorm:"type:varchar(20);default:'pending';check:status IN ('pending','paid','failed');not null"`
+	PaidAt    *time.Time
+	CreatedAt time.Time
+
+	User User
+	Tier SubscriptionTier `gorm:"foreignKey:TierID"`
+}
